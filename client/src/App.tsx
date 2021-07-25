954da0436a6cd9b9647d9bb3c9fa78fb429459d1
Joshua Wilson <wilson.jd022@gmail.com>
2021-07-24 17:00:25 -0700

    added react-router-dom router and home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,19 @@
 import React from "react";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
-function App() {
-  return <></>;
+interface AppProps {}
+
+function App(props: AppProps): JSX.Element {
+  return (
+    <>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </>
+  );
 }
 
 export default App;
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
 import { createStore, applyMiddleware } from "redux";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
@@ -10,9 +11,11 @@ const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById("root")
 );
