f6fe4e8b61100c200f5f237a8e09816d315977d4
Joshua Wilson <wilson.jd022@gmail.com>
2021-07-25 15:21:57 -0700

    add a form for the new roomdialog for validation

--- client/src/components/NewChatDialog/index.tsx
+++ client/src/components/NewChatDialog/index.tsx
@@ -10,7 +10,7 @@ import useMediaQuery from "@material-ui/core/useMediaQuery";
 import Grid from "@material-ui/core/Grid";
 import Typography from "@material-ui/core/Typography";
 import Avatar from "@material-ui/core/Avatar";
-import { useState } from "react";
+import React, { useState } from "react";
 import { useTheme } from "@material-ui/core/styles";
 import { StoreState } from "../../reducers";
 import { connect } from "react-redux";
@@ -57,13 +57,16 @@ const _NewChatDialog = (props: DialogProps): JSX.Element => {
     }
   };
 
-  const handleSubmit = () => {
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
     const newChat: ChatPartial = {
       id: Math.floor(Math.random() * 100),
       name: values.name,
       imageUrl: values.imageUrl,
       lastUpdated: new Date(),
     };
+
     props.addChatPartial(newChat);
     props.closeNewChat();
     reset();
@@ -87,64 +90,78 @@ const _NewChatDialog = (props: DialogProps): JSX.Element => {
         onClose={handleClose}
         aria-labelledby="responsive-dialog-title"
       >
-        <DialogTitle id="responsive-dialog-title">
-          {"Start A New Chat"}
-        </DialogTitle>
-        <DialogContent sx={{ m: 5 }}>
-          <FormControl sx={{ width: "100%" }} variant="standard">
-            <InputLabel htmlFor="name">Chat Name</InputLabel>
-            <Input
-              id="name"
-              type="text"
-              value={values.name}
-              onChange={handleChange("name")}
-            />
-          </FormControl>
-          <FormControl sx={{ marginY: 5, width: "100%" }} variant="standard">
-            <Grid
-              container
-              direction="row"
-              justifyContent="space-around"
-              alignItems="center"
-            >
-              <Grid item>
-                <Avatar sx={{ width: 56, height: 56 }} src={values.imageUrl} />
+        <form onSubmit={handleSubmit}>
+          <DialogTitle id="responsive-dialog-title">
+            {"Start A New Chat"}
+          </DialogTitle>
+          <DialogContent sx={{ m: 5 }}>
+            <FormControl sx={{ width: "100%" }} variant="standard">
+              <InputLabel htmlFor="name">Chat Name</InputLabel>
+              <Input
+                id="name"
+                type="text"
+                required
+                value={values.name}
+                onChange={handleChange("name")}
+              />
+            </FormControl>
+            <FormControl sx={{ marginY: 5, width: "100%" }} variant="standard">
+              <Grid
+                container
+                direction="row"
+                justifyContent="space-around"
+                alignItems="center"
+              >
+                <Grid item>
+                  <Avatar
+                    sx={{ width: 56, height: 56 }}
+                    src={values.imageUrl}
+                  />
+                </Grid>
+                <Grid item>
+                  <Typography variant="h6" component="p">
+                    Chat Image
+                  </Typography>
+                  <input
+                    onChange={handleImageUpload}
+                    accept="image/*"
+                    id="chatImage"
+                    type="file"
+                  />
+                </Grid>
               </Grid>
-              <Grid item>
-                <Typography variant="h6" component="p">
-                  Chat Image
-                </Typography>
-                <input
-                  onChange={handleImageUpload}
-                  accept="image/*"
-                  id="chatImage"
-                  type="file"
+            </FormControl>
+          </DialogContent>
+          <DialogActions>
+            <Grid container justifyContent="space-between">
+              <Button
+                autoFocus
+                size="large"
+                color="error"
+                variant="contained"
+                onClick={handleClose}
+              >
+                Cancel
+              </Button>
+              <Button
+                color="primary"
+                autoFocus
+                size="large"
+                variant="contained"
+              >
+                <Input
+                  sx={{
+                    color: "inherit",
+                    cursor: "pointer",
+                  }}
+                  disableUnderline={true}
+                  type="submit"
+                  value="CREATE"
                 />
-              </Grid>
+              </Button>
             </Grid>
-          </FormControl>
-        </DialogContent>
-        <DialogActions>
-          <Grid container justifyContent="space-between">
-            <Button
-              autoFocus
-              size="large"
-              color="error"
-              variant="contained"
-              onClick={handleClose}
-            >
-              Cancel
-            </Button>
-            <Button
-              autoFocus
-              size="large"
-              variant="contained"
-              onClick={handleSubmit}
-            >
-              Create
-            </Button>
-          </Grid>
-        </DialogActions>
+          </DialogActions>
+        </form>
       </Dialog>
     </>
   );
